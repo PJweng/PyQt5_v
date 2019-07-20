06d7c66483b7987494c7908f97a110a89233bdb7
PJweng <heartpjweng@sina.com>
2019-07-20 21:33:05 +0800

    v3

--- v1.py
+++ v1.py
@@ -1,27 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
-
-
-class Demo(QWidget):
-
-    def __init__(self):
-        super(Demo, self).__init__()
-        self.user_label = QLabel('Username:', self)
-        self.pwd_label = QLabel('Password:', self)
-
-        self.v_layout = QVBoxLayout()  # 1
-        self.v_layout.addWidget(self.user_label)  # 2
-        self.v_layout.addWidget(self.pwd_label)  # 3
-
-        self.setLayout(self.v_layout)  # 4
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    demo = Demo()
-    demo.show()
-    sys.exit(app.exec_())
+    btn = QPushButton("Button")
+    btn.show()
+    a = app.exec_()
+    print(a)
+    sys.exit(a)
 
-    
-    #加油
-    #一定一定
--- v3.py
+++ v3.py
@@ -5,7 +5,13 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
 class Demo(QWidget):
     def __init__(self):
         super(Demo, self).__init__()
+        self.resize(600, 600)
+
         self.label = QLabel('Lable', self)
+        self.btn = QPushButton('Button', self)
+
+        self.label.move(20, 0)
+        self.btn.move(50, 50)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
